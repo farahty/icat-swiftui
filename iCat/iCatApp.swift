54da8eab480594ad7d7e92aca20aa5b515890e6a
Nimer Farahty <nimer@farahty.com>
2022-07-18 22:20:50 +0300

    Initial Commit

---
+++ iCat/iCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCatApp.swift
+//  iCat
+//
+//  Created by Nimer Farahty on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
